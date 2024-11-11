71d77bd3a81c06c74b3128d3ce2de0d889faea93
Suraj Singh <suraj49470@gmail.com>
2024-11-11 14:48:21 +0530

    micro services docker compose added

---
+++ streaming-transcoding-service/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const http = require("node:http");
+const express = require("express");
+const app = express();
+const httpServer = http.createServer(app);
+const PORT = process.env.PORT || 5001;
+
+app.get('/healthz',(req,res) => {
+    res.status(200).send('ok')
+})
+
+httpServer
+  .listen(PORT)
+  .on("listening", () => console.log(`app is running on port: ${PORT}`))
+  .on("error", (error) => console.error(error));
